41bea65cd12618e944431e8d30194c4d02b8ee92
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-10-25 17:26:20 +0200

    Add controller and model for the relative db Table.

---
+++ app/Http/Controllers/TrainController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Train;
+use Illuminate\Http\Request;
+
+class TrainController extends Controller
+{
+    public function index() {
+        $trains = Train::all();
+        dd($trains);
+
+        return view("pages.trains.index", compact("trains"));
+    }
+}
