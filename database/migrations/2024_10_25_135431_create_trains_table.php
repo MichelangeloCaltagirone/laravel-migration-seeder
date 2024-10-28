faf3ad68c6c5a58282c73abac7f54eec8f19b90e
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-10-28 17:19:38 +0100

    Reconfigured the db via migration and SQL query, shown in page the data after.

--- database/migrations/2024_10_25_135431_create_trains_table.php
+++ database/migrations/2024_10_25_135431_create_trains_table.php
@@ -20,31 +20,32 @@ return new class extends Migration
         Numero Carrozze
         In orario
         Cancellato
-INSERT INTO `trains` (`id`, `Azienda`, `Stazione di arrivo`, `Stazione di partenza`, `Orario di arrivo`, `Orario di partenza`, `Codice Univoco`, `Numero Carrozze`, `In Orario`, `Cancellato` ) VALUES
-(1, 'Trenitalia', 'Roma', 'Milano', '14:12', '10:55', '4527898760', '4', true, false),
-(2, 'Trenitalia', 'Palermo', 'Milano', '12:12', '09:55', '4567898360', '30', true, false),
-(3, 'Trenitalia', 'Catania', 'Milano', '15:12', '11:55', '4563898760', '34', true, false),
-(4, 'Trenitalia', 'Napoli', 'Milano', '16:12', '10:50', '4267878760', '24', true, false),
-(5, 'Trenitalia', 'Roma', 'Como', '17:52', '10:30', '4567898761', '34', true, false),
-(6, 'Trenitalia', 'Mantova', 'Milano', '21:13', '12:23', '4567598760', '34', true, false),
-(7, 'Trenitalia', 'Roma', 'Milano', '20:45', '14:05', '1567898760', '14', true, false),
-(8, 'Trenitalia', 'Genova', 'Milano', '23:12', '10:55', '4568898760', '64', true, false),
-(9, 'Trenitalia', 'Torino', 'Milano', '17:17', '11:55', '4567878760', '74', true, false),
-(10, 'Trenitalia', 'Vercelli', 'Milano', '18:18', '15:55', '4167898760', '34', true, false);
+INSERT INTO `trains` (`id`, `Azienda`, `Stazione_di_arrivo`, `Stazione_di_partenza`, `Orario_di_arrivo`, `Orario_di_partenza`, `Codice_Univoco`, `Numero_Carrozze`, `In_Orario`, `Cancellato`, `data_partenza`) VALUES
+(1, 'Trenitalia', 'Roma', 'Milano', '14:12', '10:55', '4527898760', '4', true, false, DATE_ADD(CURDATE())),
+(2, 'Trenitalia', 'Palermo', 'Milano', '12:12', '09:55', '4567898360', '30', true, false, DATE_ADD(CURDATE())),
+(3, 'Trenitalia', 'Catania', 'Milano', '15:12', '11:55', '4563898760', '34', true, false, "2013-10-23"),
+(4, 'Trenitalia', 'Napoli', 'Milano', '16:12', '10:50', '4267878760', '24', true, false, DATE_ADD(CURDATE())),
+(5, 'Trenitalia', 'Roma', 'Como', '17:52', '10:30', '4567898761', '34', true, false, "2023-11-24"),
+(6, 'Trenitalia', 'Mantova', 'Milano', '21:13', '12:23', '4567598760', '34', true, false, "2023-05-09"),
+(7, 'Trenitalia', 'Roma', 'Milano', '20:45', '14:05', '1567898760', '14', true, false, DATE_ADD(CURDATE())),
+(8, 'Trenitalia', 'Genova', 'Milano', '23:12', '10:55', '4568898760', '64', true, false, "2023-11-20"),
+(9, 'Trenitalia', 'Torino', 'Milano', '17:17', '11:55', '4567878760', '74', true, false, DATE_ADD(CURDATE())),
+(10, 'Trenitalia', 'Vercelli', 'Milano', '18:18', '15:55', '4167898760', '34', true, false, "2013-11-21");
      */
     public function up(): void
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->string("Azienda", 100);
-            $table->string("Stazione di arrivo", 100);
-            $table->string("Stazione di partenza", 100);
-            $table->time("Orario di arrivo", precision: 2);
-            $table->time('Orario di partenza', precision: 2);
-            $table->char("Codice Univoco", 10)->unique();
-            $table->smallInteger("Numero Carrozze")->unsigned();
-            $table->boolean("In Orario");
+            $table->string("Stazione_di_arrivo", 100);
+            $table->string("Stazione_di_partenza", 100);
+            $table->time("Orario_di_arrivo", precision: 2);
+            $table->time('Orario_di_partenza', precision: 2);
+            $table->char("Codice_Univoco", 10)->unique();
+            $table->smallInteger("Numero_Carrozze")->unsigned();
+            $table->boolean("In_Orario");
             $table->boolean("Cancellato");
+            $table->date("data_partenza");
             $table->timestamps();
         });
     }
--- resources/views/pages/trains/index.blade.php
+++ resources/views/pages/trains/index.blade.php
@@ -5,8 +5,49 @@
 
 @section("main-content")
 
-<h1>
-    Trains
-</h1>
+    <h1 class="fw-bold text-center py-4">Train List</h1>
+
+    <div class="container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Azienda</th>
+                        <th>Stazione di Arrivo</th>
+                        <th>Stazione di Partenza</th>
+                        <th>Orario di Arrivo</th>
+                        <th>Orario di Partenza</th>
+                        <th>Codice Univoco</th>
+                        <th>NUmero Carrozze</th>
+                        <th>In Orario</th>
+                        <th>Cancellato</th>
+                        <th>Data Partenza</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @forelse ( $trains as $train )
+                    <tr>
+                        <td>{{ $train->id }}</td>
+                        <td>{{ $train->Azienda }}</td>
+                        <td>{{ $train->Stazione_di_arrivo }}</td>
+                        <td>{{ $train->Stazione_di_partenza }}</td>
+                        <td>{{ $train->Orario_di_arrivo }}</td>
+                        <td>{{ $train->Orario_di_partenza }}</td>
+                        <td>{{ $train->Codice_Univoco }}</td>
+                        <td>{{ $train->Numero_Carrozze }}</td>
+                        <td>{{ $train->In_Orario }}</td>
+                        <td>{{ $train->Cancellato }}</td>
+                        <td>{{ $train->data_partenza }}</td>
+                    </tr>
+
+                    @empty
+                        <tr>
+                            <td>No Data Available</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+    </div>
 
 @endsection
