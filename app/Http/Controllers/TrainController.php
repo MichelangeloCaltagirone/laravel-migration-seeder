e3fb5326351e418071c2a8fc5611d2b567e3e74d
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-10-28 17:27:00 +0100

    Add the right filter for the Train Controller.

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class TrainController extends Controller
 {
     public function index() {
-        $trains = Train::all();
+        $trains = Train::where("data_partenza", "2024-10-28")->get();
 
 
         return view('pages.trains.index', compact("trains"));
